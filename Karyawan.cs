79d84f1b98cce791f71df1f346decc66fc133e69
AffanRidha <affan.ridha02@gmail.com>
2021-04-13 14:59:48 +0700

    origin

---
+++ Karyawan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman3392
+{
+    class Karyawan
+    {
+        public int nik { get; set; }
+        public string nama { get; set; }
+        public int gajibulanan { get; set; }
+        public int naikgaji { get; set; }
+
+        public Karyawan(string nama, int gaji, int nik)
+        {
+            this.nik = nik;
+            this.nama = nama;
+
+            if (gaji < 0)
+            {
+                this.gajibulanan = 0;
+            }
+            else
+            {
+                this.gajibulanan = gaji;
+            }
+
+            double tnp = 0;
+            tnp = 0.1 * gajibulanan;
+            naikgaji = Convert.ToInt32(tnp) * gajibulanan;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ResponsiPemrograman3392
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Karyawan karyawan1 = new Karyawan(20113333, "Fulan", 5000000);
+            Karyawan karyawan2 = new Karyawan(20114444, "Fulanah", 6000000);
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("|No.\t NIK \t  NIM \t Nama \t \t Gaji|");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine(karyawan1.nim, karyawan1.nama, karyawan1.gajibulanan);
+            Console.WriteLine(karyawan2.nim, karyawan2.nama, karyawan2.gajibulanan);
+
+            Console.WriteLine("\n Selamat Kamu mendapatkan kenaikan gaji sebesar 10% \n");
+
+            Console.WriteLine("|No.\t NIK \t  NIM \t Nama \t \t Gaji|");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine(karyawan1.nim, karyawan1.nama, karyawan1.gajibulanan);
+            Console.WriteLine(karyawan2.nim, karyawan2.nama, karyawan2.gajibulanan);
+
+            Console.ReadKey();
+        }
+    }
+}
